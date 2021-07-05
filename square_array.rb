dc5487af413587ee69b665b15d365907bcb805db
chris <cseiter@gmail.com>
2021-07-05 14:24:44 -0400

    golf answer, one line, fails "don't use collect test"

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
-  square_array = []
-  # your code here
-  array.each do |indItem|
-    square_array << indItem * indItem
-  end
-square_array
+#   square_array = []
+#   # your code here
+#   array.each do |indItem|
+#     square_array << indItem * indItem
+#   end
+# square_array
+
+array.collect {|x| x*x}
 end
