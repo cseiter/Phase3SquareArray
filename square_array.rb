7495b512e590cba6683beeb4c917d23c1899e647
chris <cseiter@gmail.com>
2021-07-05 14:17:20 -0400

    tests passed

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,8 @@
 def square_array(array)
-  new_array = []
+  square_array = []
   # your code here
   array.each do |indItem|
-    new_array << indItem * indItem
+    square_array << indItem * indItem
   end
-
-puts array
-print
-puts new_array
-
+square_array
 end
-
-square_array([2,4,6])
