9f5cb6c50c9be85aecbd6b759c6de27bb00b69e9
chris <cseiter@gmail.com>
2021-07-05 14:15:12 -0400

    square_array module calculates correctly

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
+  new_array = []
   # your code here
+  array.each do |indItem|
+    new_array << indItem * indItem
+  end
+
+puts array
+print
+puts new_array
+
 end
+
+square_array([2,4,6])
